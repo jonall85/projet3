11eb9466e10ec7dc6b7414ac8442b8b4b2e6412a
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-12 23:46:27 +0100

    Ajout du fichier de l'en tête

---
+++ Vue/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<link rel="stylesheet" media="all and (max-width: 1024px)" href="petite_resolution.css" />
+        <title>Jean Forteroche</title>
+    </head>
+
+<body>
+
+	<header>
+
+		<p>
+			<img src="screen/header.png" alt="Billet simple pour l'Alaska" id="screen_header" />
+		</p>
+	
+	
+		<nav class="menu_header">
+			<ul>
+				<li> <a href="index.php"> Accueil </a></li>
+				<li> <a href="index.php?action=list"> Roman </a></li>
+				<li> <a href="index.php?action=contact"> Contact </a></li>
+			</ul>
+		</nav>
+	
+	</header>
