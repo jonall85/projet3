ab33d33287f8dff50d06fdd1990eb40998195d7f
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-12 23:47:14 +0100

    Ajout du fichier du pied de page

---
+++ Vue/footer.php
@@ -0,0 +1,21 @@
+<!--  Pied de page  -->
+
+			<footer>
+				<span class="icon_footer">
+					<img src="screen/icon_facebook.png" alt="facebook" class="icon" />
+					<img src="screen/icon_twitter.png" alt="twitter" class="icon" />
+				</span>
+	
+				<span class="copyright">
+					<p> &copy 2018 Jean Forteroche</p>
+				</span>
+	
+				<span class="admin_link">
+					<p> <a href="index.php?action=connect">Administration </a></p>
+				</span>
+			</footer>
+
+
+		</body>
+
+	</html>
