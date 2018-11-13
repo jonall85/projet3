9f05818094320d510d2a1f80cf0f4d27a31d5edf
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-13 05:46:23 +0100

    création de la vue pour la page de connexion

---
+++ Vue/connexion.php
@@ -0,0 +1,14 @@
+<?php include('Vue/header.php'); ?>
+
+	<section class="backend">
+	
+		<p> Vous devez vous connectez pour accéder à cette page </p><br />
+	
+		<form method="post" action="Controlleur/connexion.php">
+			<p> Pseudo : <input type="text" name="pseudo_connexion" id="pseudo_connexion" required></input></p>
+			<p> Mot de passe : <input type="password" name="pass_connexion" id="pass_connexion" required></input></p>
+			<p><input type="submit" value="Connexion" /></p>
+		</form>
+	</section>
+
+	<?php include('Vue/footer.php'); ?>
