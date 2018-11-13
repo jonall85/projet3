e118c255fe51ba45694c9538fa305f75428f5c32
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-13 05:28:49 +0100

    Création du header spécifique au backend

---
+++ Vue/header-backend.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<link rel="stylesheet" media="all and (max-width: 1024px)" href="petite_resolution.css" />
+		
+        <title>Jean Forteroche</title>
+		<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+		<script>tinymce.init({ selector:'textarea'});</script>
+
+    </head>
+
+<body>
+
+	<header>
+
+		<p>
+			<img src="screen/header.png" alt="Billet simple pour l'Alaska" id="screen_header" />
+		</p>
+	
+	
+		<nav class="menu_header">
+			<ul>
+				<li> <a href="index.php"> Accueil </a></li>
+				<li> <a href="index.php?action=list"> Roman </a></li>
+				<li> <a href="Controller_contact.php"> Contact </a></li>
+			</ul>
+		</nav>
+	
+	</header>
