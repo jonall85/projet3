39e1818259fa4eb35c54b8aa7cdd522e386e28de
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-19 10:18:42 +0100

    Regroupement des fonctions dans un nouveau fichier
    Regroupement des fonctions lié au billet et au commentaire

---
+++ Controlleur/fonctions.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once('Model/billet_M.php');
+require_once('Model/commentaire_M.php');
+
+
+
+	/* Fonction des billets */
+
+	function addBillet()
+	{
+
+		$titreB = $_POST['titre_billet'];
+		$contenuB = $_POST['contenu_billet'];
+	
+		$billet = new Billet;
+		$billet->add_Billets($titreB, $contenuB);
+	
+		header('location: index.php?action=backend');
+	}
+
+
+	function deleteBillet()
+	{
+		
+
+		$setSuppBillet = new Billet(); 
+		$comSupprimer = $setSuppBillet->get_supBillet($_GET['idBillet']);
+
+		header('location: index.php?action=backend');
+	}
+
+
+	function updateBillet()
+	{
+
+		$idBilletU = $_POST['id_B'];
+		$titreU = $_POST['titre_billet'];
+		$contenuU = $_POST['contenu_billet'];
+	
+		$updateBillet = new Billet(); 
+		$updateBillet->update_billet($titreU, $contenuU, $idBilletU);
+	
+		header('location: index.php?action=backend');
+	}
+
+
+	/* Fonction des commentaires */
+
+	function addCommentaire()
+	{
+
+		$auteurC = $_POST['auteur_commentaire'];
+		$contenuC = $_POST['contenu_commentaire'];
+		$idDuBillet = $_POST['id_B'];
+	
+		$Commentaire = new commentaire;
+		$Commentaire->add_Commentaire($auteurC, $contenuC, $idDuBillet);
+	
+		header('location: index.php?action=billet&page=' . $idDuBillet . ' ');
+	}
+
+
+	function deleteCommentaire()
+	{
+
+		$setSuppCom = new commentaire(); 
+		$comSupprimer = $setSuppCom->get_supCommentaire($_GET['idCom']);
+
+		header('location: index.php?action=backend');
+	}
+
+
+	function signalerCommentaire()
+	{
+		$setSignaler = new commentaire(); 
+		$comSignaler = $setSignaler->signalement_commentaire($_GET['idCom']);
+
+		header('location: index.php?action=billet&page=' . $_GET['page'] . ' ');
+	}
+
+
+
+?>
