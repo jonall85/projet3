4fb19ec06dc804d446abb55032188e66823d3d18
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-19 10:55:51 +0100

    Mise à jour des liens dans la vue : connexion.php

--- Vue/connexion.php
+++ Vue/connexion.php
@@ -4,7 +4,7 @@
 	
 		<p> Vous devez vous connectez pour accéder à cette page </p><br />
 	
-		<form method="post" action="Controlleur/connexion.php">
+		<form method="post" action="index.php?action=setCo">
 			<p> Pseudo : <input type="text" name="pseudo_connexion" id="pseudo_connexion" required></input></p>
 			<p> Mot de passe : <input type="password" name="pass_connexion" id="pass_connexion" required></input></p>
 			<p><input type="submit" value="Connexion" /></p>
