198d8e75b23329f2af47b37a2b2a9a1ce630f5ec
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-19 10:39:48 +0100

    Modification de connexion.php et mise en objet
    ajout des méthodes "setConnexion" et "testConnexion"

--- Controlleur/connexion.php
+++ Controlleur/connexion.php
@@ -1,29 +1,62 @@
 <?php
 	
-	session_start();
+	
+class connexion{
 
-	$pseudo = 'jeanF';
-	$pass = '123456';
 
-	if($_POST && !empty($_POST['pseudo_connexion']) && !empty($_POST['pass_connexion']))
+	public function setConnexion()
 	{
-		if(($pseudo == $_POST['pseudo_connexion']) && ($pass == $_POST['pass_connexion']))
-		{
-			$_SESSION['_login'] = $pseudo;
-       		$_SESSION['_pass'] = $pass;
 
-       		header('location: ../index.php?action=backend');
+		session_start();
+
+		$pseudo = 'jeanF';
+		$pass = '123456';
+
+		if($_POST && !empty($_POST['pseudo_connexion']) && !empty($_POST['pass_connexion']))
+		{
+			if(($pseudo == $_POST['pseudo_connexion']) && ($pass == $_POST['pass_connexion']))
+			{
+				$_SESSION['_login'] = $pseudo;
+       			$_SESSION['_pass'] = $pass;
+
+       			header('location: index.php?action=backend');
+			}
+
+			else
+			{
+				header('location: index.php?action=connect');
+			}
 		}
 
 		else
 		{
-			header('location: ../index.php?action=connect');
+			header('location: index.php?action=connect');
 		}
 	}
 
-	else
+
+	public function testConnexion()
+	{
+
+		session_start();
+
+		$pseudo = 'jeanF';
+		$pass = '123456';
+
+		if(!isset($_SESSION['_login']) || !isset($_SESSION['_pass']))
 		{
-			header('location: ..index.php?action=connect');
+			header('location: index.php?action=connect');
 		}
+
+		else
+		{
+			if(($pseudo != $_SESSION['_login']) || ($pass != $_SESSION['_pass']))
+			{
+				header('location: index.php?action=connect');
+			}
+		}
+	}
+}
+
 
 ?>
