f926d11945fbca3a30cb6670e2d5a84d33c45bde
jonall85 <42961716+jonall85@users.noreply.github.com>
2018-11-19 16:54:06 +0100

    hash du mot de pass

--- Controlleur/connexion.php
+++ Controlleur/connexion.php
@@ -10,11 +10,11 @@ class connexion{
 		session_start();
 
 		$pseudo = 'jeanF';
-		$pass = '123456';
+		$pass = '$2y$10$HMfhP9iHH0G3F91rm9h6beyjA.YDL5SBYuC8QvsIUE0uJVC1qo0lW';
 
 		if($_POST && !empty($_POST['pseudo_connexion']) && !empty($_POST['pass_connexion']))
 		{
-			if(($pseudo == $_POST['pseudo_connexion']) && ($pass == $_POST['pass_connexion']))
+			if(($pseudo == $_POST['pseudo_connexion']) && (password_verify($_POST['pass_connexion'], $pass)))
 			{
 				$_SESSION['_login'] = $pseudo;
        			$_SESSION['_pass'] = $pass;
@@ -41,7 +41,7 @@ class connexion{
 		session_start();
 
 		$pseudo = 'jeanF';
-		$pass = '123456';
+		$pass = '$2y$10$HMfhP9iHH0G3F91rm9h6beyjA.YDL5SBYuC8QvsIUE0uJVC1qo0lW';
 
 		if(!isset($_SESSION['_login']) || !isset($_SESSION['_pass']))
 		{
